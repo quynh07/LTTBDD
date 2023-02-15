31c9dcbdc39b41d8abda3f7edf92f455e0e6d6ec
quynh07 <125440577+quynh07@users.noreply.github.com>
2023-02-15 14:50:24 +0700

    Add files via upload

---
+++ hello.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+void main(){
+  //Cau1
+  print("Bui Thi Quynh");
+  // Cau 2
+  print("Hello I am John Doe ");
+  print(" Hello I'm John Doe ");
+  // Cau 3,4
+  int p=7, t=100, r=6;
+  double Formula = (p*t*r)/100;
+  print (Formula);
+  // Cau 5
+  print("Nhap so can tinh: ");
+  int num = int.parse(stdin.readLineSync()!);
+  int x = num*num;
+  print("Binh phuong vua nhap la: $x");
+  // cau6
+  print(" Nhap ho cua ban: ");
+  String?ho = stdin.readLineSync();
+  print(" Nhap ten cua ban: ");
+  String? ten = stdin.readLineSync();
+  print("Ten cua ban la: $ho $ten");
+  // cau 7
+  print("Nhap a: ");
+  int a = int.parse(stdin.readLineSync()!);
+  print("Nhap b: ");
+  int b = int.parse(stdin.readLineSync()!);
+  double thuong = a/b;
+ int sodu = a%b;
+  print("a/b = $thuong");
+  print("a%b = $sodu");
+  // Cau8
+  print (" Chuong trinh doi cho hai so");
+  void doicho(){
+    int temp = a;
+    a = b;
+    b = temp;
+  }
+  doicho();
+  print("So a moi la: $a");
+  print(" So b moi la: $b");
+  //Cau9
+  String text = "Nhap chuoi co khoang trang: ";
+  String newText = text.replaceAll(' ','');
+  print ( newText);
+  
+  }
+
+
+
+
